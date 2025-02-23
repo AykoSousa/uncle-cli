06fe1e2b2667d295537bd675b5d429f21cca9db5
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2025-02-23 16:14:28 -0300

    feat: add integration with gemini api

---
+++ cmd/core/gemini/main.go
@@ -0,0 +1,27 @@
+package gemini
+
+import (
+    "context"
+    "log"
+
+	"github.com/AykoSousa/uncle-cli/cmd/handlers"
+    "github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func Prompt(API_KEY string, modelName string, prompt string) {
+    ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(API_KEY))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel(modelName)
+	resp, err := model.GenerateContent(ctx, genai.Text(prompt))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handlers.Print(resp)
+}
