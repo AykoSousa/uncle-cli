f676f9d55eeb6f6d7d27a4c6b774574c9c547ad7
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2025-02-23 16:14:59 -0300

    feat: add function to get response from gemini model

---
+++ cmd/handlers/printResponse.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"fmt"
+
+	"github.com/google/generative-ai-go/genai"
+)
+
+func Print(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part)
+			}
+		}
+	}
+	fmt.Println("---")
+}
